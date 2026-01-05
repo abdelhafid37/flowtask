d115e7133f9c15f6fd353cbf83ed60a1bbed0e27
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-01-05 18:22:11 +0100

    add task model

---
+++ backend/models/Task.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: false,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    dueDate: {
+      type: Date,
+      required: false,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
